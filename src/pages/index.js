585c2afb0d8aa0aab68bd51fd7ad523bf2d88fda
OgutuJoseph <onyango.joseph@yahoo.com>
2022-07-20 14:09:48 +0300

    Using styling components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Layout from '../components/Layout';
+import * as styles from '../styles/home.module.css';
+import { Link } from 'gatsby';
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
-          <h2>Design</h2>
+          <h2 >Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer.</p>
-        </div>
+          <Link className={styles.btn} to='/projects'>My Projects</Link>
+        </div> 
       </section>
     </Layout>
   )
